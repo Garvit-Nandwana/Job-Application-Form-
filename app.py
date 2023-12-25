a44661baccb1998fda28c5843417c89ee1c1585c
garvitnandwana <nandwanagarvit4@gmail.com>
2023-12-25 13:50:37 +0530

    getting user input

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        date = request.form['date']
+        email = request.form['email']
+        occupation = request.form['occupation']
     return render_template("index.html")
 
 
